e779d0a3eca1871ab775d8a2dbdd4a1cd8903c7c
Marco ObaID <marco.obaid@gmail.com>
2020-07-22 21:14:02 -0500

    Added Plasma, Autologin, and Liveuser.

--- airootfs/root/customize_airootfs.sh
+++ airootfs/root/customize_airootfs.sh
@@ -13,3 +13,12 @@ passwd -d root
 
 sed -i 's/#\(PermitRootLogin \).\+/\1yes/' /etc/ssh/sshd_config
 sed -i "s/#Server/Server/g" /etc/pacman.d/mirrorlist
+
+#oblinux
+systemctl enable pacman-init.service choose-mirror.service
+systemctl set-default graphical.target
+systemctl enable sddm.service
+
+groupsoblinux="adm,audio,disk,floppy,log,network,optical,rfkill,storage,video,wheel,sys"
+useradd -m -g users -G $groupsoblinux -s /bin/bash liveuser
+passwd -d liveuser
