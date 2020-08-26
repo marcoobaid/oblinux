77312ad5e2d43b08aa626584b05b60edd02b9ba2
Marco ObaID <marco.obaid@gmail.com>
2020-08-26 08:36:51 -0500

    Update for archiso 47-1

--- build.sh
+++ build.sh
@@ -4,6 +4,10 @@
 
 set -e -u
 
+#printf '\n[%s] WARNING: %s\n\n' "mkarchiso" "build.sh scripts are deprecated! Please use mkarchiso directly." >&2
+#_buildsh_path="$(realpath -- "$0")"
+#exec mkarchiso "$@" "${_buildsh_path%/*}"
+
 #oblinux
 sudo ./cleanup*
 sudo chown -R root airootfs/
