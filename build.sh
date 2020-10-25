f787d1ec430cf96fa9a14f052c0bb01f6c2bd916
Marco ObaID <marco.obaid@gmail.com>
2020-10-25 15:51:40 -0500

    Test Push

--- build.sh
+++ build.sh
@@ -2,8 +2,6 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-set -e -u
-
 printf '\n[%s] WARNING: %s\n\n' "mkarchiso" "build.sh scripts are deprecated! Please use mkarchiso directly." >&2
 _buildsh_path="$(realpath -- "$0")"
 exec mkarchiso "$@" "${_buildsh_path%/*}"
